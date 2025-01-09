8f4927786c8057b3cc8617da750b893d7ff89274
Garga <sahagarga000@gmail.com>
2025-01-09 14:03:20 +0530

    first

--- obj/Debug/net8.0/FeedBackMC.AssemblyInfo.cs
+++ obj/Debug/net8.0/FeedBackMC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FeedBackMC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de69d9b0405f273c61c27607734217bdf9b12e98")]
 [assembly: System.Reflection.AssemblyProductAttribute("FeedBackMC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FeedBackMC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
